c26c6992686c48bd2e5d424e28be9879f85055ea
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-03-24 14:11:08 +0000

    tjweigjewogogwjoiweogjo; esj; hr hoerhtrj r;hjto hjr;o roi; mhrt tmjcgnrvdst1txh1hxfaelsvrntd

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,8 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		// visualization test case
+		// bubbleSortString(new String[] {"zZ","q","c","b","a","y","z","j"});
 	}
 
 	public static void q1() {
@@ -24,4 +25,34 @@ public class PracticeProblem {
 		//Write question 5 code here
 	}
 
+	// public static void print(String[] yo) {
+	// 	for (int i = 0; i < yo.length; i++) {
+	// 		System.out.print(yo[i] + " ");
+	// 	}
+	// 	System.out.println();
+	// }
+	public static void bubbleSortString(String[] strings) {
+
+		int finalIteration = strings.length - 1;
+		for (int i = 0; i < finalIteration; i++) {
+			for (int n = 0; n < finalIteration - i; n++) {
+				// visualize the bubbling
+				// print(strings);
+
+				String bubble = strings[n];
+				String water = strings[n + 1];
+				
+				if (bubble.toLowerCase().compareTo(water.toLowerCase()) > 0) {
+					strings[n] = water;
+					strings[n + 1] = bubble;
+				// } else {
+				// 	break;
+				}
+			}
+		}
+	}
 }
+
+/*
+Create a method called bubbleSortString(String[] strings) which takes in a string array and sorts it in alphabetical order.  Your bubble sort should ignore casing.  return nothing.
+*/
